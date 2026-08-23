a326412498027155948d9324c0820f27e894f496
rodrigo-j-goncalves <romadryn@gmail.com>
2026-08-23 15:52:08 +0200

    fix: escapar \n en lines.join del template JS

--- generate_html.py
+++ generate_html.py
@@ -491,7 +491,7 @@ HTML_TEMPLATE = """\
         ? '"' + s.replace(/"/g, '""') + '"' : s;
     };
     const lines = [cols.join(','), ...DATA.map(d => cols.map(c => esc(d[c])).join(','))];
-    const blob = new Blob([lines.join('\n')], { type: 'text/csv;charset=utf-8;' });
+    const blob = new Blob([lines.join('\\n')], { type: 'text/csv;charset=utf-8;' });
     const url  = URL.createObjectURL(blob);
     const a    = document.createElement('a');
     a.href = url; a.download = 'terremotos_ign.csv';
